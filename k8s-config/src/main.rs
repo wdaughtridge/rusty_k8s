0c1988601e33fee5dc527b1d3a07d0fe9367b441
Will Daughtridge <wdaughtridge@gmail.com>
2025-06-03 00:07:08 -0400

    feat

--- k8s-config/src/main.rs
+++ k8s-config/src/main.rs
@@ -1,15 +1,37 @@
-use k8s::k8s::io::{api::core::v1::Pod, apimachinery::pkg::apis::meta::v1::ObjectMeta};
-
-mod k8s {
+mod proto {
     tonic::include_proto!("k8s");
 }
+
+use proto::k8s::io::{
+    api::core::v1::{Container, Pod, PodSpec},
+    apimachinery::pkg::apis::meta::v1::ObjectMeta,
+};
 
 fn main() {
     let mut my_pod = Pod::new();
 
-    let mut meta = ObjectMeta::new();
-    meta.set_name("my-pod".to_string());
-    meta.set_namespace("devops".to_string());
+    let mut metadata = ObjectMeta::new();
+    metadata.set_name("my-pod".to_string());
+    metadata.set_namespace("devops".to_string());
+    my_pod.set_metadata(metadata);
 
-    my_pod.set_metadata(meta);
+    let mut my_container = Container::new();
+    my_container.set_name("busy-pod".to_string());
+    my_container.set_image("busybox:latest".to_string());
+    my_container.set_image_pull_policy("Always".to_string());
+    my_container.set_command(
+        [
+            "/bin/sh".to_string(),
+            "-c".to_string(),
+            "'while true; do echo hello; sleep 120; done'".to_string(),
+        ]
+        .to_vec(),
+    );
+
+    let mut my_spec = PodSpec::new();
+    my_spec.set_containers([my_container].to_vec());
+
+    my_pod.set_spec(my_spec);
+
+    println!("{}", serde_json::to_string_pretty(&my_pod).unwrap());
 }
--- k8s-macro/src/lib.rs
+++ k8s-macro/src/lib.rs
@@ -9,6 +9,52 @@ pub fn k8s_resource(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
 
+    // let new = if let syn::Data::Struct(data) = &input.data {
+    //     data.fields.iter().map(|field| {
+    //         let field_name = &field.ident;
+    //         let field_type = &field.ty;
+
+    //         if field_name.clone().unwrap() == Ident::new("metadata", field_name.span()) {
+    //             let syn::Type::Path(field_type) = field_type.clone() else {
+    //                 panic!("Metadata field type not path");
+    //             };
+
+    //             // let Some(last_segment) = field_type.path.segments.last() else {
+    //             //     panic!("Prob shouldn't happen");
+    //             // };
+
+    //             // let PathArguments::AngleBracketed(path_args) = &last_segment.arguments else {
+    //             //     panic!("Prob shouldn't happen either?");
+    //             // };
+
+    //             println!("{:?}", field_type.path);
+
+    //             return quote! {
+    //                 pub fn new(name: &'static str, namespace: &'static str) -> Self {
+    //                     Self {
+    //                         metadata: Some(#field_type {
+    //                             name: Some(name.to_string()),
+    //                             namespace: Some(namespace.to_string()),
+    //                             ..std::default::Default::default()
+    //                         }),
+    //                         ..std::default::Default::default()
+    //                     }
+    //                 }
+    //             };
+    //         } else {
+    //             return quote! {
+    //                 pub fn new() -> Self {
+    //                     Self {
+    //                         ..std::default::Default::default()
+    //                     }
+    //                 }
+    //             };
+    //         }
+    //     })
+    // } else {
+    //     return TokenStream::new();
+    // };
+
     let setters = if let syn::Data::Struct(data) = &input.data {
         data.fields.iter().map(|field| {
             let field_name = &field.ident;
@@ -20,37 +66,35 @@ pub fn k8s_resource(input: TokenStream) -> TokenStream {
                 field_name.span(),
             );
 
-            if let syn::Type::Path(type_path) = field_type {
-                // Is this an option?
-                let option_path = type_path.path.segments.iter().find(|s| s.ident == "Option");
-                if option_path.is_some() {
-                    // We should be able to get generics from it if so
-                    if let PathArguments::AngleBracketed(generic_t) =
-                        &option_path.unwrap().arguments
-                    {
-                        if let GenericArgument::Type(syn::Type::Path(type_path)) =
-                            &generic_t.args[0]
-                        {
-                            quote! {
-                                pub fn #setter_name(&mut self, value: #type_path) {
-                                    self.#field_name = Some(value);
-                                }
-                            }
-                        } else {
-                            panic!("No generics for Option<T>");
-                        }
-                    } else {
-                        panic!("No generics for Option<T>");
+            let syn::Type::Path(type_path) = field_type else {
+                return quote! {};
+            };
+
+            // Is this an option?
+            let option_path = type_path.path.segments.iter().find(|s| s.ident == "Option");
+
+            if option_path.is_some() {
+                // We should be able to get generics from it if so
+                let PathArguments::AngleBracketed(generic_t) = &option_path.unwrap().arguments
+                else {
+                    panic!("No generics for Option<T>");
+                };
+
+                let GenericArgument::Type(syn::Type::Path(type_path)) = &generic_t.args[0] else {
+                    panic!("No generics for Option<T>");
+                };
+
+                return quote! {
+                    pub fn #setter_name(&mut self, value: #type_path) {
+                        self.#field_name = Some(value);
                     }
-                } else {
-                    quote! {
-                        pub fn #setter_name(&mut self, value: #field_type) {
-                            self.#field_name = value;
-                        }
-                    }
-                }
+                };
             } else {
-                quote! {}
+                return quote! {
+                    pub fn #setter_name(&mut self, value: #field_type) {
+                        self.#field_name = value;
+                    }
+                };
             }
         })
     } else {
